6762b6cfd06e651be471e88bb581983e4b76f615
cintia divanti agustin <2511500051@mahasiswa.atmaluhur.ac.id>
2026-09-25 20:33:16 +0700

    menambahkan file data mahasiswa

--- pertemuan2/api/mahasiswa.php
+++ pertemuan2/api/mahasiswa.php
@@ -3,19 +3,14 @@
 require_once "../config.php";
 require_once "../helpers/response.php";
 
-$q = "SELECT m.Id, m.Nama, m.Nim, j.Nama_jurusan AS jurusan
-FROM mahasiswa m
-LEFT JOIN jurusan j ON m.Jurusan_id = j.Id
-ORDER BY m.Id DESC";
+$q = "SELECT m.id, m.nama, m.nim, j.nama_jurusan AS jurusan 
+FROM mahasiswa m LEFT JOIN jurusan j on m.jurusan_id = j.id ORDER BY m.id DESC";
 
-$r = mysqli_query($GLOBALS['koneksi'], $q);
+$r = mysqli_query($koneksi, $q);
 
-if (!$r) {
-    sendResponse(false, "query gagal: " . mysqli_error($GLOBALS['koneksi']), null, 500);
-}
+if(!$r) sendResponse(false, "query gagal: " . mysqli_error($koneksi), null, 500);
 
 $data = [];
-
 while ($row = mysqli_fetch_assoc($r)) {
     $data[] = $row;
 }
